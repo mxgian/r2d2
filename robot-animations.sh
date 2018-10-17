7389e998b347c6335135572b4d0f69cf66b57bfb
minhg <minh@minhgiang.com>
2018-10-17 17:04:09 -0400

    Update robot-animations.sh

--- robot-animations.sh
+++ robot-animations.sh
@@ -5,7 +5,7 @@
 declare -a arr=("scared" "no" "bipod" "angry" "confident" "tripod" "sad" "alarm" "laugh" "surprise" "chatty" "yes" "happy" "ionblast" "annoyed" "excited")
 
 # robot needs to be r2d2 or q5. no error checking
-robot = $1
+robot = "$1"
 for i in "${arr[@]}"
 do 
 echo "$i"
