69b65073b7e670c104bdab9092f5fff6158452fb
minhg <minh@minhgiang.com>
2018-10-16 23:01:25 -0400

    x

---
+++ monitor_ifttt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Check your paths here
+/home/pi/r2d2/dropbox_uploader.sh list  > /tmp/ifttt
+
+File="/tmp/ifttt"
+
+# Monitor to turn L1 on - check your paths here
+if grep -q r2d2.txt "$File"; then
+   /home/pi/r2d2/dropbox_uploader.sh delete r2d2.txt && /bin/rm /tmp/ifttt && echo "Time: $(date) got email" >> /tmp/L1.log && /home/pi/r2d2/r2d2.py r2d2 -c happy
+fi
