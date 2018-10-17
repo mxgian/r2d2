aaf798900bd7c23070bd364f80b4a800924a3881
minhg <minh@minhgiang.com>
2018-10-16 23:05:03 -0400

    x

---
+++ loop-test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script opens 4 terminal windows.
+
+i="0"
+
+while [ $i -lt 60 ]
+do
+	/home/pi/r2d2/monitor_ifttt.sh
+	sleep(60)
+i=$[$i+1]
+done
--- monitor_ifttt.sh
+++ monitor_ifttt.sh
@@ -4,7 +4,7 @@
 /home/pi/r2d2/dropbox_uploader.sh list  > /tmp/ifttt
 
 File="/tmp/ifttt"
-
+echo "checking for file"
 # Monitor to turn L1 on - check your paths here
 if grep -q r2d2.txt "$File"; then
    /home/pi/r2d2/dropbox_uploader.sh delete r2d2.txt && /bin/rm /tmp/ifttt && echo "Time: $(date) got email" >> /tmp/L1.log && /home/pi/r2d2/r2d2.py r2d2 -c happy
