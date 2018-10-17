ed52ca858a7317424a3e6135803a2a65e0bda5b2
minhg <minh@minhgiang.com>
2018-10-17 16:52:59 -0400

    x

---
+++ robot-animations.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script opens 4 terminal windows.
+
+declare -a arr=("scared" "no" "bipod" "angry" "confident" "tripod" "sad" "alarm" "laugh" "surprise" "chatty" "yes" "happy" "ionblast" "annoyed" "excited")
+
+# robot needs to be r2d2 or q5. no error checking
+robot = $1
+for i in "${arr[@]}"
+do 
+ /home/pi/r2d2/r2d2.py $robot -c $i
+done
