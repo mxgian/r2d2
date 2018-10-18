ecfc7aa1c189fe910383a5a80a8517cb72f6a44d
Minh <pi@raspberrypi>
2018-10-18 10:27:02 -0400

    x

--- monitor_ifttt.sh
+++ monitor_ifttt.sh
@@ -4,8 +4,11 @@
 /home/pi/r2d2/dropbox_uploader.sh list  > /tmp/ifttt
 
 File="/tmp/ifttt"
-echo "checking for file"
+echo "checking for file:$(date)" >> /tmp/L1.log
 # Monitor to turn L1 on - check your paths here
 if grep -q r2d2.txt "$File"; then
    /home/pi/r2d2/dropbox_uploader.sh delete r2d2.txt && /bin/rm /tmp/ifttt && echo "Time: $(date) got email" >> /tmp/L1.log && /home/pi/r2d2/r2d2.py r2d2 -c happy
+fi
+if grep -q r2-abh.txt "$File"; then
+   /home/pi/r2d2/dropbox_uploader.sh delete r2-abh.txt && /bin/rm /tmp/ifttt && echo "Time: $(date) got email" >> /tmp/L1.log && /home/pi/r2d2/r2d2.py r2d2 -c laugh
 fi
