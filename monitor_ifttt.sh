083011051d00a99a01e522bc9e58c41782981689
Minh <pi@raspberrypi>
2018-10-18 14:49:51 -0400

    xx

--- monitor_ifttt.sh
+++ monitor_ifttt.sh
@@ -17,11 +17,21 @@ fi
 
 # this section cleans up the dir if there are files left over, for example if the monitor script crashes or mails come in too fast and we have multiple files (1,2,3), in this case dropbox adds n+1 file instead of creating the r2d2.txt and it won't trigger the script.  
 # this code would need to be cleaned up to handle multiple use cases
-/home/pi/r2d2/dropbox_uploader.sh list   | awk '{ print $NF }' | grep r2 > /tmp/checklist
+ /home/pi/r2d2/dropbox_uploader.sh list   | awk  '{ NF1=NF-1; print  $NF1 " " $NF }' > /tmp/checklist
 
-for i in $(cat /tmp/checklist);
+ IFS=$'\r\n' GLOBIGNORE='*' command eval  'XYZ=($(cat /tmp/checklist))'
+for i in "${XYZ[@]}"
 do 
-echo "deleting $i"
-/home/pi/r2d2/dropbox_uploader.sh delete $i
+echo "deleting ${i}"
+#/home/pi/r2d2/dropbox_uploader.sh delete $i
 done
 
+#/home/pi/r2d2/dropbox_uploader.sh list   | awk '{ print $NF }' | grep r2 > /tmp/checklist
+
+#for i in $(cat /tmp/checklist);
+#do 
+#echo "deleting $i"
+#/home/pi/r2d2/dropbox_uploader.sh delete $i
+#done
+
+/bin/rm /tmp/checklist
--- test_list.sh
+++ test_list.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-/home/pi/r2d2/dropbox_uploader.sh list   | awk '{ print $NF }' > /tmp/checklist
+#/home/pi/r2d2/dropbox_uploader.sh list   | awk '{ print $NF }' > /tmp/checklist
+ /home/pi/r2d2/dropbox_uploader.sh list   | awk  '{ NF1=NF-1; print  $NF1 " " $NF }' > /tmp/checklist
 
-for i in $(cat /tmp/checklist);
+ IFS=$'\r\n' GLOBIGNORE='*' command eval  'XYZ=($(cat /tmp/checklist))'
+for i in "${XYZ[@]}"
 do 
-echo "deleting $i"
-/home/pi/r2d2/dropbox_uploader.sh delete $i
+echo "deleting ${i}"
+#/home/pi/r2d2/dropbox_uploader.sh delete $i
 done
-
