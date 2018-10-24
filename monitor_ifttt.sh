8c0ed25fa9ab15baf077f2b9a3bd95cdd489da0f
minhg <minh@minhgiang.com>
2018-10-24 16:26:34 -0400

    Update monitor_ifttt.sh

--- monitor_ifttt.sh
+++ monitor_ifttt.sh
@@ -7,8 +7,8 @@ File="/tmp/ifttt"
 
 # do some checks, if we ran an alert in the last 5 mins, do not run the alert
 lastrun="/tmp/checkstamp"
-rightnow = /bin/date +%s
-echo "lastostamp $lastrun , $rightnow"
+rightnow=/bin/date +%s
+echo "lastostamp ${lastrun} , ${rightnow}"
 # Monitor to turn L1 on - check your paths here
 if grep -q r2d2.txt "$File"; then
    /home/pi/r2d2/dropbox_uploader.sh delete r2d2.txt && /bin/rm /tmp/ifttt && echo "Time: $(date) got email" >> /tmp/L1.log && /home/pi/r2d2/r2d2.py r2d2 -c happy
